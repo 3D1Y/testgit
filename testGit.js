af8d23871dc03e4031feec76f20f21bc7e21a2d3
3D1Y <hi_xieyufeng@163.com>
2021-07-11 23:12:36 +0800

    really change

--- testGit.js
+++ testGit.js
@@ -2,6 +2,8 @@
 function consoleAnyThing() {
   console.log('输出任何')
   console.log('this is a change')
-  console.log('')
+  console.log('test soft')
+  console.log('do a change')
+  console.log('我做了一些改变')
 }
 consoleAnyThing()
